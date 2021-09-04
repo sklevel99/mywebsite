47dd0d94b04a000433e2612faa6b7decd644a20d
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-09-04 14:40:52 -0400

    skills section done

---
+++ app/javascript/packs/skil.js
@@ -0,0 +1,26 @@
+const skillsContent = document.getElementsByClassName('skills_content'),
+skillsHeader  = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills_content skills_close'
+  }
+
+  if(itemClass === 'skills_content skills_close'){
+    this.parentNode.className = 'skills_content skills_open'
+  }
+}
+
+// skillsHeader.forEach((el) => {
+//   el.addEventListener('click', toggleSkills)
+// })
+skillsHeader.forEach(item => item.addEventListener('click', toggleSkills))
+
+
+
+export { skillsContent, skillsHeader, itemClass };
+
+
+
