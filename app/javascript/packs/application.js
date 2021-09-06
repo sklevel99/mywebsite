79f67550c8c1fcdba8988f374b40d838305b94d9
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-09-05 21:07:09 -0400

    qualifications section fully done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,25 +36,6 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
 });
 
-const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
-
-tabs.forEach(tab =>{
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target)
-
-    tabContents.forEach(tabContent =>{
-      tabContent.classList.remove('qualification_active')
-    })
-    target.classList.add('qualification_active')
-
-    tabs.forEach(tab =>{
-      tab.classList.remove('qualification_active')
-    })
-    tab.classList.add('qualification_active')
-  })
-})
-
 
 
 
