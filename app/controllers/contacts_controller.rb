7dfffd5cb6fb4eb500d527e57600804f851f9252
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-09-07 16:14:31 -0400

    contact me partially done

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,18 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request= request
+    if @contact.deliver
+      flash[:notice] = "Your message has been sent. I'll get back to you soon."
+      redirect_to root_path
+    else
+      flash[:error] = 'Message cannot be sent'
+      render :new
+      redirect_to root_path
+    end
+  end
+end
